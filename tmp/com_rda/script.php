c47ea9cc08bae525ee5ba960bda52d143c480b4b
David <David@David-PC.homenet.telecomitalia.it>
2014-01-16 06:47:19 +0100

    dd

--- tmp/com_rda/script.php
+++ tmp/com_rda/script.php
@@ -7,7 +7,7 @@
  * @copyright	Copyright (C) 2005 - 2010 Open Source Matters, Inc. All rights reserved.
  * @author		Christophe Demko
  * @link		http://joomlacode.org/gf/project/rda_1_6/
- * @license		License GNU General Public License version 2 or later
+ * @license		License GNU General Public License version 2 or later..
  */
 // No direct access to this file
 defined('_JEXEC') or die('Restricted access');
